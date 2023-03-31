c80006b155d879035c6488b923f85e445c59c6c1
Maxim Farafonov <anderson.utuber@gmail.com>
2023-03-31 19:25:16 +0600

    Initialized project

---
+++ src/main/java/com/project/orderfoodsproject/OrderFoodsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.orderfoodsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFoodsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFoodsProjectApplication.class, args);
+    }
+
+}
